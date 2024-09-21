83887cd1de1d4edb201cf1c509e66e9f2757ba19
freeddyy0124 <82694987+freeddyy0124@users.noreply.github.com>
2024-09-21 16:42:59 -0500

    Added logic to get human choice

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
+function getHumanChoice() {
+    let choice = prompt("Please enter rock, paper, or scissors:");
+    return choice.toLowerCase();
+}
+
+console.log(getHumanChoice());
 
 // Continue with step 3
